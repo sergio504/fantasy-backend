02ec73b570b39224b456224af4384cbb7affc986
Sergio <sergiocavia@gmail.com>
2026-06-05 12:51:03 +0200

    Error, localhost estaba hardcodedao

--- src/prismaClient.ts
+++ src/prismaClient.ts
@@ -2,7 +2,7 @@ import { PrismaClient } from '@prisma/client'
 import { PrismaPg } from '@prisma/adapter-pg'
 
 const adapter = new PrismaPg({
-  connectionString: 'postgresql://fantasy_user:fantasy_pass@localhost:5432/fantasy_futbol',
+  connectionString: process.env.DATABASE_URL!,
 })
 
 export const prisma = new PrismaClient({ adapter })
